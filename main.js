fa1258ee3c3ad35eafa43c7c005785e0e28b489b
Robert Bishop <rbishop2006@gmail.com>
2020-02-13 18:21:45 -0800

    checking

--- main.js
+++ main.js
@@ -3,4 +3,31 @@ $(document).ready(function() {
 
   memory.createGame()
   memory.start()
-})
+  $(".cards").flip()
+  var num1 = ""
+  var num2 = ""
+  $(".cards").on("click", ".front", function(e) {
+    e.preventDefault()
+
+    value = $(this)
+      .find("+ .back")
+      .html()
+    if (num1 == "") {
+      num1 += value
+    } else {
+      num2 += value
+    }
+    if (num1 !== "" && num2 !== "" && num1 === num2) {
+      num1 = ""
+      num2 = ""
+      console.log("match")
+    } else if (num1 !== "" && num2 !== "" && num1 !== num2) {
+      num1 = ""
+      num2 = ""
+
+      console.log("flip over")
+    }
+
+    console.log(num1, num2)
+  })
+ 
