4bfb12e1f994bf64d7f88017d67ad2804473092c
Robert Bishop <robertbishop@robertbishopdev.local>
2020-06-01 09:11:38 -0700

    added a comment on player.js

--- player.js
+++ player.js
@@ -1,3 +1,6 @@
+/*create players for the game, takes a name as a parameter, Player object has the argument.name and empty.hand.  
+Then, the hand property receives and is assigned a card.*/
+
 class Player {
   constructor(name) {
     this.name = name
